c79ff25399077b4bcff9d297808b67178893c735
Diego Pollheim <diegopollheim@gmail.com>
2022-05-12 22:27:43 -0300

    day-night 01

--- src/components/pages/Sobre.jsx
+++ src/components/pages/Sobre.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import {GoogleMap, useGoogleMap, LoadScript, useJsApiLoader, useLoadScript} from "@react-google-maps/api";
+import React, {useEffect, useLayoutEffect} from "react";
 import InfoIss from "../InfoIss";
 
 export default function Sobre() {
+  const {isLoaded, loadError} = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyDdk4QLWPmk3KbK79iSwnYsFYYvFLFDaak", // ,
+    // ...otherOptions
+  });
+  
+  const containerStyle = {
+    width: "900px",
+    height: "500px",
+  };
+
+  const center = {
+    lat: -27,
+    lng: -48,
+  };
+
+  if (isLoaded) {
+    return (
+      <div id="map" style={{width: "900px", height: "500px"}}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={4} />;
+      </div>
+    );
+  }
+
   return (
     <>
       <h2>Sobre</h2>
-  <InfoIss />
     </>
   );
 }
